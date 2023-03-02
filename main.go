24bf1ae90daefce5d8884ebc123a5269c880d51f
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-03-02 14:32:24 +0700

    add: login user endpoint

--- main.go
+++ main.go
@@ -2,7 +2,14 @@ package main
 
 import (
 	"log"
+	"pos/auth"
 	"pos/database"
+	"pos/handler"
+	"pos/helper"
+	"pos/user"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -11,5 +18,31 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
-	log.Println(connection)
+	// auth
+	authService := auth.NewAuthService()
+
+	// user
+	userRepo := user.NewRepository(connection)
+	userService := user.NewService(userRepo)
+	userHandler := handler.NewHandlerUser(userService, authService)
+
+	// init service
+	r := gin.Default()
+	r.Use(cors.Default())
+
+	// api versioning
+	apiV1 := r.Group("/api/v1")
+
+	// route
+	apiV1.POST("/login", userHandler.Login)
+
+	
+	env, err := helper.GetENV(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := r.Run(env["DOMAIN"]); err != nil {
+		log.Fatal(err)
+	}
 }
