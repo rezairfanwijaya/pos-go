6fac7a08559d4b6ccb3a1b874f9f0a659805571a
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-03-02 14:52:31 +0700

    add: user by userID

--- user/repository.go
+++ user/repository.go
@@ -4,6 +4,7 @@ import "gorm.io/gorm"
 
 type IRepository interface {
 	FindByUsername(username string) (User, error)
+	FindByUserID(userID int) (User, error)
 }
 
 type repository struct {
@@ -18,6 +19,16 @@ func (r *repository) FindByUsername(username string) (User, error) {
 	var user User
 
 	if err := r.db.Where("username = ? ", username).Find(&user).Error; err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
+func (r *repository) FindByUserID(userID int) (User, error) {
+	var user User
+
+	if err := r.db.Where("id = ?", userID).Find(&user).Error; err != nil {
 		return user, err
 	}
 
--- user/service.go
+++ user/service.go
@@ -7,17 +7,18 @@ import (
 
 type IService interface {
 	Login(input InputUserLogin) (User, error)
+	GetUserByID(userID int) (User, error)
 }
 
-type service struct {
+type Service struct {
 	userRepo IRepository
 }
 
-func NewService(userRepo IRepository) *service {
-	return &service{userRepo}
+func NewService(userRepo IRepository) *Service {
+	return &Service{userRepo}
 }
 
-func (s *service) Login(input InputUserLogin) (User, error) {
+func (s *Service) Login(input InputUserLogin) (User, error) {
 	userByUsername, err := s.userRepo.FindByUsername(input.Username)
 	if err != nil {
 		return userByUsername, err
@@ -38,4 +39,20 @@ func (s *service) Login(input InputUserLogin) (User, error) {
 	}
 
 	return userByUsername, nil
+}
+
+func (s *Service) GetUserByID(userID int) (User, error) {
+	userByUserID, err := s.userRepo.FindByUserID(userID)
+	if err != nil {
+		return userByUserID, err
+	}
+
+	if userByUserID.ID == 0 {
+		return userByUserID, fmt.Errorf(
+			"id %v not found",
+			userID,
+		)
+	}
+
+	return userByUserID, nil
 }
