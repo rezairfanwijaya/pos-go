77a75c0b2b59aabbde68ad65fe43a125631e272d
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-03-02 11:50:09 +0700

    add: hash password

--- helper/helper.go
+++ helper/helper.go
@@ -2,6 +2,7 @@ package helper
 
 import (
 	"github.com/joho/godotenv"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func GetENV(path string) (env map[string]string, err error) {
@@ -11,4 +12,26 @@ func GetENV(path string) (env map[string]string, err error) {
 	}
 
 	return env, nil
+}
+
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+	if err != nil {
+		return "", err
+	}
+
+	return string(hashedPassword), nil
+}
+
+func VerifyPassword(rawPassword, hashedPassword []byte) (err error) {
+	err = bcrypt.CompareHashAndPassword(
+		hashedPassword,
+		rawPassword,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
