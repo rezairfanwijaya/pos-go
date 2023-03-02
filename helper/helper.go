210df14a54806b6eff551ee1b5926dc86172eec2
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-03-02 14:31:36 +0700

    add: format response api and error binding

--- helper/helper.go
+++ helper/helper.go
@@ -1,9 +1,30 @@
 package helper
 
 import (
+	"github.com/go-playground/validator/v10"
 	"github.com/joho/godotenv"
 	"golang.org/x/crypto/bcrypt"
 )
+
+type responseAPI struct {
+	Meta meta        `json:"meta"`
+	Data interface{} `json:"data"`
+}
+
+type meta struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+}
+
+func GenerateResponse(status int, message string, data interface{}) responseAPI {
+	return responseAPI{
+		Meta: meta{
+			Status:  status,
+			Message: message,
+		},
+		Data: data,
+	}
+}
 
 func GetENV(path string) (env map[string]string, err error) {
 	env, err = godotenv.Read(path)
@@ -34,4 +55,12 @@ func VerifyPassword(rawPassword, hashedPassword []byte) (err error) {
 	}
 
 	return nil
+}
+
+func FormatingErrorBinding(err error) (errBinding []string) {
+	for _, e := range err.(validator.ValidationErrors) {
+		errBinding = append(errBinding, e.Error())
+	}
+
+	return errBinding
 }
