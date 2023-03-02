cc5083e1b2771112b6bfcd29606f22feac1e4089
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-03-02 17:43:05 +0700

    add: handler delete transaction

--- handler/transaction.go
+++ handler/transaction.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"pos/helper"
 	"pos/transaction"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -52,5 +53,39 @@ func (h *transactionHandler) NewTransaction(c *gin.Context) {
 	)
 
 	c.JSON(http.StatusOK, response)
+}
 
+func (h *transactionHandler) DeleteTransactionByID(c *gin.Context) {
+	id := c.Param("id")
+
+	transactionID, err := strconv.Atoi(id)
+	if err != nil || transactionID < 1 {
+		response := helper.GenerateResponse(
+			http.StatusBadRequest,
+			"invalid id transaction",
+			"id must be int and grather than 0",
+		)
+
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	if err := h.transactionService.DeleteTransactionByID(transactionID); err != nil {
+		response := helper.GenerateResponse(
+			http.StatusNotFound,
+			"failed delete transaction",
+			err.Error(),
+		)
+
+		c.JSON(http.StatusNotFound, response)
+		return
+	}
+
+	response := helper.GenerateResponse(
+		http.StatusOK,
+		"success",
+		"success delete transaction",
+	)
+
+	c.JSON(http.StatusOK, response)
 }
